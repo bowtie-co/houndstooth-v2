b81da416a9e6dda8cd26b6c38a46969f6f85b6b5
Brennan <brennan@bowtie.co>
2020-07-24 16:40:35 -0700

    Replace AsyncSelect with Select

--- src/molecules/Repo/Select.jsx
+++ src/molecules/Repo/Select.jsx
@@ -1,13 +1,11 @@
 import React, { useEffect, useState } from 'react';
-import PropTypes from 'prop-types';
 import { navigate } from 'hookrouter';
-import AsyncSelect from 'react-select/async';
+import Select from 'react-select';
 
 export const RepoSelect = (props) => {
   const { repo, repos, translate, className = '' } = props;
 
-  // const repoOptions = Array.isArray(repos) ? repos.map(repo => ({ value: repo, label: repo.full_name })) : [];
-
+  const [ loading, setLoading ] = useState(true);
   const [ selected, setSelected ] = useState(!!repo && repo.id);
   const [ placeholder, setPlaceholder ] = useState(!!repo ? repo.full_name : translate('general.loading'));
 
@@ -16,59 +14,25 @@ export const RepoSelect = (props) => {
     navigate(`/${e.full_name}/collections?ref=${e.default_branch || 'master'}`);
   }
 
-  const customFilter = (option, text) => {
-    console.log(option, text)
-    if (!!text) {
-      debugger;
-      // option.full_name.toLowerCase().includes(text.toLowerCase());
-    } else {
-      return true;
-    }
-  }
-
   useEffect(() => {
     if (!repo) {
-      setPlaceholder(!repos ? `${translate('general.loading')} ...` : translate('repos.select'));
+      setPlaceholder(!repos.length ? `${translate('general.loading')} ...` : translate('repos.select'));
     }
-  }, [repos])
+    setLoading(!repos.length);
+  }, [repo, repos, translate])
 
   return (
-    <AsyncSelect
+    <Select
       className={`RepoSelect ${className}`}
-      // TODO: @Brennan - replace with loadOptions promise function
-      defaultOptions={repos}
+      options={repos}
       getOptionLabel={option => option.full_name}
       getOptionValue={option => option}
       isOptionSelected={option => selected === option.id}
       placeholder={placeholder}
-      isSearchable
-      // filterOption={customFilter}
+      isLoading={loading}
+      isDisabled={loading}
       onChange={handleSelect}
       autoFocus
     />
-    // <FieldContainer
-    //   // async
-    //   className='repo-select'
-    //   clearable={false}
-    //   name='select-repo'
-    //   type={'select'}
-    //   placeholder={!repos ? 'Loading ...' : 'Select Repo'}
-    //   value={`${baseRoute}`}
-    //   valueKey={'full_name'}
-    //   labelKey='full_name'
-    //   disabled={!repos}
-    //   options={asyncRepoList}
-    //   onChange={(e) => {
-    //     history.push({ pathname: `/${e.target.value}/collections`, search: `?ref=${e.target.default_branch || 'master'}` });
-    //   }}
-    // />
   );
-};
-
-RepoSelect.propTypes = {
-  stagedFiles: PropTypes.array,
-  repo: PropTypes.object,
-  message: PropTypes.string,
-  setMessage: PropTypes.func,
-  pushToGithub: PropTypes.func
 };
