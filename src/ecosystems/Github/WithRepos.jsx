f92a3aa1f375fb9fc09fb69e3095237b8e7c5fc0
Brennan <brennan@bowtie.co>
2020-07-24 17:15:16 -0700

    Cache repo list

--- src/ecosystems/Github/WithRepos.jsx
+++ src/ecosystems/Github/WithRepos.jsx
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from 'react';
+import { storage } from '../../lib';
 import {
   WithLoader,
   WithChildren,
@@ -7,21 +8,33 @@ import {
 export const WithGithubRepos = ({ children, ...props }) => {
   console.debug('WithGithubRepos', { children, props });
 
-  const [ repos, setRepos ] = useState([]);
+  const [ repos, setRepos ] = useState(storage.get('all_repos') || []);
   const [ loading, setLoading ] = useState(true);
   const { github } = props;
 
   useEffect(() => {
-    setLoading(true);
+    if (!repos.length) {
+      setLoading(true);
 
-    github.repos().then(data => {
-      setRepos(data);
-      setLoading(false);
-    }).catch(err => {
-      console.warn(err);
-      setLoading(false);
-    });
-  }, [ github ]);
+      // TODO: @Brennan - remove temporary arguments
+      github.repos().then(results => {
+        const flattenedRepos = [];
+        results.forEach(result => {
+          const { id, default_branch, description, full_name, name, owner, updated_at } = result;
+          flattenedRepos.push(Object.assign({}, {
+            id, default_branch, description, full_name, name, owner, updated_at
+          }));
+        });
+        storage.set('all_repos', flattenedRepos);
+        setRepos(flattenedRepos);
+        debugger;
+        setLoading(false);
+      }).catch(err => {
+        console.warn(err);
+        setLoading(false);
+      });
+    }
+  }, [ github, repos, loading ]);
 
   return (
     <WithLoader isLoading={loading} defer>
