4428ea2712bb7920adc3f1efa3b91eddce76b877
Brennan <brennan@bowtie.co>
2020-08-13 16:57:02 -0600

    Optimize RepoSelect w/ async & limited results

--- src/molecules/Repo/Select.jsx
+++ src/molecules/Repo/Select.jsx
@@ -1,12 +1,25 @@
 import React, { useEffect, useState } from 'react';
-import Select from 'react-select';
+import AsyncSelect from 'react-select/async';
 
 export const RepoSelect = (props) => {
   const { location, repo, repos, translate, className = '' } = props;
 
   const [ loading, setLoading ] = useState(true);
   const [ selected ] = useState(!!repo && repo.id);
+  const [ defaultOptions ] = useState(repos.length ? repos.slice(0, 100) : []);
   const [ placeholder, setPlaceholder ] = useState(!!repo ? repo.full_name : translate('general.loading'));
+
+  const search = (inputValue) => {
+    return inputValue ?
+      repos.filter(r =>r.full_name.toLowerCase().includes(inputValue.toLowerCase())).slice(0, 100)
+      : defaultOptions;
+  };
+
+  const loadOptions = (inputValue, callback) => {
+    setTimeout(() => {
+      callback(search(inputValue));
+    }, 100);
+  };
 
   const handleSelect = (e) => {
     location.href = `/${e.full_name}/collections`;
@@ -17,12 +30,13 @@ export const RepoSelect = (props) => {
       setPlaceholder(!repos.length ? `${translate('general.loading')} ...` : translate('repos.select'));
     }
     setLoading(!repos.length);
-  }, [repo, repos, translate])
+  }, [repo, repos, translate]);
 
   return (
-    <Select
+    <AsyncSelect
       className={`NavSelect ${className}`}
-      options={repos}
+      loadOptions={loadOptions}
+      defaultOptions={defaultOptions}
       getOptionLabel={option => option.full_name}
       getOptionValue={option => option}
       isOptionSelected={option => selected === option.id}
