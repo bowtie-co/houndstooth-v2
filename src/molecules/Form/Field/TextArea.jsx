93da2ec95eb6d94db34b86dad7050ce391788a93
brendenthornton <thorntonbrenden@gmail.com>
2020-08-22 16:58:11 -0600

    manually passed clearable prop with a string value, and removed ...rest props since they weren't needed for Input component

--- src/molecules/Form/Field/TextArea.jsx
+++ src/molecules/Form/Field/TextArea.jsx
@@ -3,10 +3,12 @@ import { Input } from 'reactstrap';
 import { FormGroup } from '../';
 
 export const FormFieldTextArea = (props) => {
-  const { value, edited, className = '', ...rest } = props;
+  const { value, edited, className = '', clearable, ...rest } = props;
+  const { onChange } = rest;
+
   return (
     <FormGroup className={`${className} ${edited ? 'success-highlight' : ''}`} {...rest}>
-      <Input rows='5' type='textarea' value={value || ''} {...rest} />
+      <Input rows='5' type='textarea' value={value || ''} clearable={clearable.toString()} onChange={onChange} />
     </FormGroup>
   );
 };
