dee45e4b73b5c8973cb0e811c2d86066fcb70393
Dustin-Har <dustin@bowtie.co>
2022-05-06 17:49:06 -0400

    Add default hint value

--- src/molecules/Form/Field/Hint.jsx
+++ src/molecules/Form/Field/Hint.jsx
@@ -3,16 +3,21 @@ import { FormGroup } from '../';
 import { AppIcon } from '../../../atoms';
 
 export const FormFieldHint = (props) => {
-	const { value, edited, className = '' } = props;
-	
-	return (
-		<FormGroup className={`${className} ${edited ? 'success-highlight' : ''}`} {...props}>
-			<div className='flex'>
-				<pre>
-					<AppIcon className='far fa-lightbulb' />
-					{value}
-				</pre>
-			</div>
-		</FormGroup>
-	);
+  const { value, edited, className = '' } = props;
+  let hintValue = value;
+
+  if (!value || value.trim() === '') {
+    hintValue = 'Add a hint value in your files front matter'
+  }
+
+  return (
+    <FormGroup className={`${className} ${edited ? 'success-highlight' : ''}`} {...props}>
+      <div className='flex'>
+        <pre>
+          <AppIcon className='far fa-lightbulb' />
+          {hintValue}
+        </pre>
+      </div>
+    </FormGroup>
+  );
 };
