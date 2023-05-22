ca8f96261d7c075ca729c4486057c1b09c81332d
Dustin Harbaugh <dustin@bowtie.co>
2023-05-22 17:23:10 -0400

    make airbrake a conditional for testing

--- src/lib/airbrake.js
+++ src/lib/airbrake.js
@@ -8,27 +8,30 @@ const {
   REACT_APP_AIRBRAKE_KEY
 } = process.env;
 
-const airbrake = new Notifier({
-  projectId: REACT_APP_AIRBRAKE_ID,
-  projectKey: REACT_APP_AIRBRAKE_KEY
-});
-
-const determineEnv = () => {
-  return 'development';
-};
-
-airbrake.addFilter((notice) => {
-  if (notice) {
-    notice.context.profile = REACT_APP_PROFILE;
-    notice.context.version = REACT_APP_VERSION || '0';
-    notice.context.environment = REACT_APP_ENV || determineEnv();
-
-    if (notice.context.environment === 'development') {
-      notice.context.severity = 'warn';
+let airbrake = null;
+if(REACT_APP_AIRBRAKE_ID && REACT_APP_AIRBRAKE_KEY) {
+  airbrake = new Notifier({
+    projectId: REACT_APP_AIRBRAKE_ID,
+    projectKey: REACT_APP_AIRBRAKE_KEY
+  });
+  
+  const determineEnv = () => {
+    return 'development';
+  };
+  
+  airbrake.addFilter((notice) => {
+    if (notice) {
+      notice.context.profile = REACT_APP_PROFILE;
+      notice.context.version = REACT_APP_VERSION || '0';
+      notice.context.environment = REACT_APP_ENV || determineEnv();
+      
+      if (notice.context.environment === 'development') {
+        notice.context.severity = 'warn';
+      }
     }
-  }
-
-  return notice;
-});
+    
+    return notice;
+  });
+}
 
 export { airbrake };
